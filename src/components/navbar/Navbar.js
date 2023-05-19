9c630b5ef95687e98738b2caf9193b637863d583
nareshmnk20 <mnknaresh2012@gmail.com>
2023-05-19 21:57:54 +0530

    first commit

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,14 @@
+
+
+import "./Navbar.css";
+import { NavLink } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <div className="nav">
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/cart">Cart</NavLink>
+    </div>
+  );
+};
+
+export default Navbar;
